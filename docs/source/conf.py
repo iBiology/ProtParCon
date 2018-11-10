99a5dca601a44db5677e45885450d5ed101afc00
FEI YUAN <yuanfeifuzzy@gmail.com>
2018-11-10 13:04:50 -0600

    Rebuilt docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-print(os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
